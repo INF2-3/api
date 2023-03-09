aa639f6d3e197519a10362f4a5904c7943ec2bfa
Franca <franca.baars@student.nhlstenden.com>
2023-03-09 15:59:50 +0100

    Get info from the file

--- api/src/main/java/com/quintor/api/mongoConnection/FileController.java
+++ api/src/main/java/com/quintor/api/mongoConnection/FileController.java
@@ -1,14 +1,15 @@
 package com.quintor.api.mongoConnection;
 
-import ch.qos.logback.core.model.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 @RestController
 public class FileController {
+    @Autowired
     FileService fileService;
 
     @Autowired
@@ -17,15 +18,27 @@ public class FileController {
     }
 
     @PostMapping("/add")
-    public String addFile(@RequestParam("file")MultipartFile file) throws IOException {
-        String id = fileService.addFile("test", file);
-        return "test";
+    public String test(@RequestParam("file")MultipartFile file) throws IOException {
+        String fileName = file.getOriginalFilename();
+        String test1 = file.getName();
+        String test2 = file.getContentType();
+        String test3 = file.toString();
+        long size = file.getSize();
+        String test = file.getInputStream().toString();
+        String test5 = Arrays.toString(file.getBytes());
+
+        return test5;
+//                "filename: " + fileName + " name: " + test1 + " content type: " + test2 + " bestand to string:  "+ test3;
     }
 
-    @GetMapping("/get/{id}")
-    public String getFile(@PathVariable String id, Model model) {
-        Mt940 mt940 = fileService.getMt940(id);
-        return "files";
-    }
+//    public ResponseEntity<?> addFile(@RequestParam("file")MultipartFile file) throws IOException {
+//        return new ResponseEntity<>(fileService.addFile(file), HttpStatus.OK);
+//    }
+
+//    @GetMapping("/get/{id}")
+//    public String getFile(@PathVariable String id, Model model) {
+//        Mt940 mt940 = fileService.getMt940(id);
+//        return "files";
+//    }
 
 }
--- api/src/main/java/com/quintor/api/mongoConnection/FileService.java
+++ api/src/main/java/com/quintor/api/mongoConnection/FileService.java
@@ -1,8 +1,12 @@
 package com.quintor.api.mongoConnection;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
 import org.bson.BsonBinarySubType;
 import org.bson.types.Binary;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.gridfs.GridFsOperations;
+import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -12,17 +16,22 @@ import java.io.IOException;
 public class FileService {
 
     @Autowired
-    private FileRepository fileRepository;
+    private GridFsTemplate template;
+    @Autowired
+    private GridFsOperations operations;
 
-    public String addFile(String title, MultipartFile file) throws IOException {
-        Mt940 mt940 = new Mt940(title);
-        mt940.setFile(new Binary(BsonBinarySubType.BINARY, file.getBytes()));
-        mt940 = fileRepository.insert((mt940));
-        return mt940.getId();
+//    private FileRepository fileRepository;
+
+    public String addFile(MultipartFile file) throws IOException {
+        DBObject metadata = new BasicDBObject();
+        metadata.put("filesize", file.getSize());
+
+        Object fileId = template.store(file.getInputStream(), file.getOriginalFilename(), file.getContentType(), metadata);
+        return fileId.toString();
     }
 
-    public Mt940 getMt940(String id) {
-        return fileRepository.findById(id).get();
-    }
+//    public Mt940 getMt940(String id) {
+//        return fileRepository.findById(id).get();
+//    }
 
 }
