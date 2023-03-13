43fd74e46a786d42bc362d1270fcecc25d22e770
Franca <franca.baars@student.nhlstenden.com>
2023-03-13 13:52:59 +0100

    Deleted unused code apiApplication

--- api/src/main/java/com/quintor/api/ApiApplication.java
+++ api/src/main/java/com/quintor/api/ApiApplication.java
@@ -1,45 +1,14 @@
 package com.quintor.api;
 
-import com.quintor.api.mongoConnection.Student;
-import com.quintor.api.mongoConnection.StudentRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import java.util.List;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
 //@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 public class ApiApplication {
-
-	@Autowired
-	StudentRepository repository;
-
-	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
-	}
-
-	@Bean
-	CommandLineRunner runner(StudentRepository repository) {
-		return args -> {
-			Student student = new Student("Franca", "Baars", "francabaars@gmail.com");
-
-			Object[] test = repository.findAll().toArray();
-			System.out.println(repository.findAll().get(0).getFirstName());
-
-//			repository.insert(student);
-		};
-	}
-
-	private List<Student> getAllFiles(StudentRepository repository) {
-		List<Student> alles = repository.findAll();
-		return alles;
-
-	}
-
-
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
 }
