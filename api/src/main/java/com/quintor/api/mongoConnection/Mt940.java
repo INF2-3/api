3905916014cfa28445695dddd34ad59d614f2489
Franca <franca.baars@student.nhlstenden.com>
2023-03-13 14:08:13 +0100

    added uploaduser to Mt940 class

--- api/src/main/java/com/quintor/api/mongoConnection/Mt940.java
+++ api/src/main/java/com/quintor/api/mongoConnection/Mt940.java
@@ -3,16 +3,17 @@ package com.quintor.api.mongoConnection;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.persistence.Id;
-import java.time.LocalDate;
-@Document(collection = "files")
+
+@Document(collection = "file")
 public class Mt940 {
     @Id
     private String id;
     private String file;
-    private LocalDate uploadDate;
+    private int uploadUser;
 
-    public Mt940(String file) {
+    public Mt940(String file, int uploadUser) {
         this.file = file;
+        this.uploadUser = uploadUser;
     }
 
     public String getId() {
@@ -23,19 +24,19 @@ public class Mt940 {
         this.id = id;
     }
 
-    public LocalDate getUploadDate() {
-        return this.uploadDate;
-    }
-
-    public void setUploadDate(LocalDate uploadDate) {
-        this.uploadDate = uploadDate;
-    }
-
     public String getFile() {
         return this.file;
     }
 
     public void setFile(String file) {
         this.file = file;
+    }
+
+    public int getUploadUser() {
+        return this.uploadUser;
+    }
+
+    public void setUploadUser(int uploadUser) {
+        this.uploadUser = uploadUser;
     }
 }
