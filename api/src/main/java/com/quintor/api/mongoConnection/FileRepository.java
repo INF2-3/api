0d8a0033fded8fee3d2bf7f345855447afb038b2
Franca <franca.baars@student.nhlstenden.com>
2023-03-13 13:53:19 +0100

    deleted filerepository

--- api/src/main/java/com/quintor/api/mongoConnection/FileRepository.java
+++
@@ -1,6 +0,0 @@
-package com.quintor.api.mongoConnection;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface FileRepository extends MongoRepository<Mt940, String> {
-}
