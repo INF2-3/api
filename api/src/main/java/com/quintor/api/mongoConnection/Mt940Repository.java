69780b76c579fe8782b3f793e27761aecd156b47
Franca <franca.baars@student.nhlstenden.com>
2023-03-13 13:56:36 +0100

    deleted unused method findMT940 by id

--- api/src/main/java/com/quintor/api/mongoConnection/Mt940Repository.java
+++ api/src/main/java/com/quintor/api/mongoConnection/Mt940Repository.java
@@ -5,5 +5,4 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.Optional;
 
 public interface Mt940Repository extends MongoRepository<Mt940, String> {
-    Optional<Mt940> findMt940ById(String id);
 }
