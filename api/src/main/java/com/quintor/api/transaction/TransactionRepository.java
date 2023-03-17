c8d87e2ae217f78593ab72bea58bc9612ed79ef8
Franca <franca.baars@student.nhlstenden.com>
2023-03-17 11:27:10 +0100

    added TransactionRepository

---
+++ api/src/main/java/com/quintor/api/transaction/TransactionRepository.java
@@ -0,0 +1,7 @@
+package com.quintor.api.transaction;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository extends JpaRepository<Transaction, String> {
+
+}
