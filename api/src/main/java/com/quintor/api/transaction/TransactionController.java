504a922b07f2f92ddfa6437ba85ab754e2cabb15
Franca <franca.baars@student.nhlstenden.com>
2023-03-21 16:09:14 +0100

    send transaction to frontend

--- api/src/main/java/com/quintor/api/transaction/TransactionController.java
+++ api/src/main/java/com/quintor/api/transaction/TransactionController.java
@@ -1,5 +1,8 @@
 package com.quintor.api.transaction;
 
+import com.quintor.api.category.Category;
+import com.quintor.api.description.Description;
+import org.apache.catalina.webresources.Cache;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,23 +14,19 @@ import java.util.List;
 @RestController
 @RequestMapping("api/transaction/")
 public class TransactionController {
-//    private final TransactionService transactionService;
-//
-//    public TransactionController(TransactionService transactionService) {
-//        this.transactionService = transactionService;
-//    }
-//
-//    public TransactionService getTransactionService(){
-//        return this.transactionService;
-//    }
-
-
     @GetMapping("/getAllTransactions")
     public List<Transaction> listAll() {
         List<Transaction> test = new ArrayList<>();
-        test.add(new Transaction(1, LocalDate.now(), 25, DebitOrCredit.CREDIT, 25, "test", 33, 54, 43));
-        test.add(new Transaction(2, LocalDate.now(), 25, DebitOrCredit.CREDIT, 25, "test", 33, 54, 43));
-        test.add(new Transaction(3, LocalDate.now(), 25, DebitOrCredit.CREDIT, 25, "test", 33, 54, 43));
+        Description description = new Description(1);
+        Category leden  = new Category(1, "Leden");
+        Category test1 = new Category(3, "Test");
+        Transaction transaction1 = new Transaction(1, LocalDate.now(), 25, DebitOrCredit.CREDIT, 25, "test", description, 54, leden);
+        description.setCharges("test");
+
+        transaction1.setOriginalDescription(description);
+        test.add(transaction1);
+        test.add(new Transaction(2, LocalDate.now(), 25, DebitOrCredit.CREDIT, 25, "test", description, 54, test1));
+        test.add(new Transaction(3, LocalDate.now(), 25, DebitOrCredit.CREDIT, 25, "test", description, 54, leden));
 
         return test;
     }
