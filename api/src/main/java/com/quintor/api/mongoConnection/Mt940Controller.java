1f4b6528067dcecca6c85d32ae743e457046d5cc
Jesse Vugteveen <jesse.vugteveen@student.nhlstenden.com>
2023-03-23 10:52:36 +0100

    started with xml schema

--- api/src/main/java/com/quintor/api/mongoConnection/Mt940Controller.java
+++ api/src/main/java/com/quintor/api/mongoConnection/Mt940Controller.java
@@ -1,11 +1,19 @@
 package com.quintor.api.mongoConnection;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.networknt.schema.JsonSchema;
+import com.networknt.schema.JsonSchemaFactory;
+import com.networknt.schema.SpecVersion;
+import com.networknt.schema.ValidationMessage;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -13,6 +21,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.Set;
 
 @RestController
 @RequestMapping("api/mt940")
@@ -63,4 +72,30 @@ public class Mt940Controller {
         InputStream stream = new FileInputStream(file);
         return new MockMultipartFile("file", file.getName(), MediaType.TEXT_HTML_VALUE, stream);
     }
+//
+//    private Set<ValidationMessage> jsonValidation(String jsonString) throws JsonProcessingException {
+//        InputStream schemaStream = Mt940Controller.class.getClassLoader().getResourceAsStream("schemas/schema.json");
+//        JsonSchema jsonSchema = JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V7).getSchema(schemaStream);
+//
+//        ObjectMapper objectMapper = new ObjectMapper();
+//
+//        JsonNode jsonNode = objectMapper.readTree(jsonString);
+//        Set<ValidationMessage> errors = jsonSchema.validate(jsonNode);
+//        String errorsCombined = "";
+//        for (ValidationMessage error : errors) {
+//            errorsCombined += error.toString() + "\n";
+//        }
+//
+//        return errors;
+////        if (errors.size() > 0) {
+////            throw new RuntimeException("bad json" + errorsCombined);
+////        }
+////
+////        return jsonString;
+//    }
+//
+//    @PostMapping("/MT940toJSONValidation")
+//    public String MT940toJSONValidation(@RequestParam("file") MultipartFile file) {
+//
+//    }
 }
