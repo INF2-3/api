a0ab7717eea261b7576f0ba2ae6ab4afb5184146
Franca <franca.baars@student.nhlstenden.com>
2023-03-24 11:16:29 +0100

    made static method to make transaction

---
+++ api/src/main/java/com/quintor/api/postgresql/AddToTransaction.java
@@ -0,0 +1,51 @@
+package com.quintor.api.postgresql;
+
+import com.quintor.api.dataobjects.Category;
+import com.quintor.api.dataobjects.DebitOrCredit;
+import com.quintor.api.dataobjects.Description;
+import com.quintor.api.dataobjects.Transaction;
+
+import java.time.LocalDate;
+
+public class AddToTransaction {
+    public static Transaction makeTransaction(int id, LocalDate valueDate, int entryDate, String debCred, double amount, String transactionCode, String referenceOwner, String institutionReference, String supplementaryDetails, int originalDescriptionId, String description, int fileId, int categoryId) {
+        DebitOrCredit debitOrCredit;
+        if (debCred.equals("c") || debCred.equals("C")) {
+            debitOrCredit = DebitOrCredit.CREDIT;
+        } else if (debCred.equals("d") || debCred.equals("D")) {
+            debitOrCredit = DebitOrCredit.DEBIT;
+        } else {
+            debitOrCredit = null;
+        }
+
+        Transaction transaction = new Transaction(id, valueDate, entryDate, debitOrCredit, amount, transactionCode, new Description(originalDescriptionId), fileId, new Category(categoryId, "test"));
+        transaction.setReferenceOwner(referenceOwner);
+        transaction.setInstitutionReference(institutionReference);
+        transaction.setSupplementaryDetails(supplementaryDetails);
+        transaction.setDescription(description);
+        return transaction;
+    }
+
+    public static Description makeDescription(int id, String returnReason, String clientReference, String endToEndReference,
+                                              String paymentInformationId, String instructionId, String mandateReference,
+                                              String creditorId, String counterpartyId, String remittanceInformation,
+                                              String purposeCode, String ultimateCreditor, String ultimateDebitor,
+                                              String exchangeRate, String charges) {
+        Description description = new Description(id);
+        description.setReturnReason(returnReason);
+        description.setClientReference(clientReference);
+        description.setEndToEndReference(endToEndReference);
+        description.setPaymentInformationId(paymentInformationId);
+        description.setInstructionId(instructionId);
+        description.setMandateReference(mandateReference);
+        description.setCreditorId(creditorId);
+        description.setCounterpartyId(counterpartyId);
+        description.setRemittanceInformation(remittanceInformation);
+        description.setPurposeCode(purposeCode);
+        description.setUltimateCreditor(ultimateCreditor);
+        description.setUltimateDebitor(ultimateDebitor);
+        description.setExchangeRate(exchangeRate);
+        description.setCharges(charges);
+        return description;
+    }
+}
