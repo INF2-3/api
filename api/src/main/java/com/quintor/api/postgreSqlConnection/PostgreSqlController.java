6369f21346c38023ebdf21a5e17aa58eca26f397
Noizeisdead <noizee829@gmail.com>
2023-03-27 11:52:08 +0200

    added test sting, connection with parser works

--- api/src/main/java/com/quintor/api/postgreSqlConnection/PostgreSqlController.java
+++ api/src/main/java/com/quintor/api/postgreSqlConnection/PostgreSqlController.java
@@ -1,5 +1,6 @@
 package com.quintor.api.postgreSqlConnection;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.quintor.api.mt940.Mt940Service;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
@@ -9,13 +10,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.swing.plaf.nimbus.State;
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 @RestController
 @RequestMapping("api/mt940/postgres")
@@ -29,7 +28,7 @@ public class PostgreSqlController {
     }
 
     @PostMapping("/insert")
-    public String i (@RequestParam("userId") int userId, @RequestParam("file") File file) throws IOException{
+    public String i (@RequestParam("userId") int userId, @RequestParam("file") File file) throws IOException, SQLException {
             //remove when using postman to test
         MultipartFile multipartFile = convertFileToMultiPartFile(file);
         if (multipartFile == null || multipartFile.isEmpty()) {
@@ -40,25 +39,91 @@ public class PostgreSqlController {
         }
         String SQL = "INSERT INTO test(naam) VALUES (?)";
         // Step 1: Establishing a Connection
-        try (Connection connection = DriverManager.getConnection(url, user, password);
+        try (Connection connection = DriverManager.getConnection(url, user, password)){
+
 
              // Step 2:Create a statement using connection object
-             PreparedStatement preparedStatement = connection.prepareStatement(SQL)) {
-            preparedStatement.setString(1, "afschrift");
+//             PreparedStatement preparedStatement = connection.prepareStatement(SQL)) {
+//            preparedStatement.setString(1, "afschrift");
+             insertInto();
 
-
-            System.out.println(preparedStatement);
+//            System.out.println(preparedStatement);
             // Step 3: Execute the query or update query
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
+//            preparedStatement.executeUpdate();
+//        }
+//        catch (SQLException e) {
 
             // print SQL exception information
-            return e.toString();
+//            return e.toString();
+            return "yes";
         }
 
 
-        return "succes";
+//        return "help";
     }
+
+
+    private void insertInto() throws SQLException {
+        Connection connection = DriverManager.getConnection(url, user, password);
+
+        String[] json = new String[]{"{\n" +
+                "    \"transactionType\": \"N\",\n" +
+                "    \"identificationCode\": \"TRF\",\n" +
+             5   "    \"amount\": \"1,56\",\n" +
+                "    \"entryDate\": \"0220\",\n" +
+                "    \"informationToAccountOwner\": {\n" +
+                "     \"paymentInformationId\": \"\",\n" +
+                "     \"ultimateCreditor\": \"\",\n" +
+                "     \"endToEndReference\": \"EV12341REP1231456T1234\",\n" +
+                "     \"ultimateDebitor\": \"\",\n" +
+                "     \"counterPartyId\": \"NL32INGB0000012345/INGBNL2A/ING BANK NV INZAKE WEB\",\n" +
+                "     \"mandateReference\": \"\",\n" +
+                "     \"charges\": \"\",\n" +
+                "     \"accountOwnerInformationInOneLine\": \"/EREF/EV12341REP1231456T1234//CNTP/NL32INGB0000012345/INGBNL2A/ING BANK NV INZAKE WEB///REMI/USTD//EV10001REP1000000T1000/\",\n" +
+                "     \"remittanceInformation\": \"USTD//EV10001REP1000000T1000\",\n" +
+                "     \"exchangeRate\": \"\",\n" +
+                "     \"returnReason\": \"\",\n" +
+                "     \"purposeCode\": \"\",\n" +
+                "     \"clientReference\": \"\",\n" +
+                "     \"instructionId\": \"\",\n" +
+                "     \"creditorId\": \"\"\n" +
+                "    }"};
+
+
+        try{
+            String sql = "INSERT INTO transaction VALUES (?, ?::JSON)";
+            PreparedStatement ps = connection.prepareStatement(sql);
+
+            for (int i=0; i<4; i++) {
+                ps.setInt (1, i+1);
+                ps.setObject (2, json[i]);
+                ps.executeUpdate();
+            }
+            connection.commit();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+//    private <PGobject> getFromDbToJson(){
+//        try{
+//            String sql = "";
+//            Connection connection = DriverManager.getConnection(url, user, password);
+//            Statement stmnt = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+//            ResultSet rs = stmnt.executeQuery(sql);
+//
+//            JSONPObject json;
+//            JSONPObject json2;
+//
+//            while(rs.next()){
+//
+//            }
+//
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+
 
     private MultipartFile convertFileToMultiPartFile(File file) throws IOException {
         if (file == null || !file.exists() || !file.isFile()) {
