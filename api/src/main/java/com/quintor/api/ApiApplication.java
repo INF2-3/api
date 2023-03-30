95ff7842f35e0b3f6ab338ba5b7fd6b85814a93f
Franca <franca.baars@student.nhlstenden.com>
2023-03-30 16:01:17 +0200

    removed unused code

--- api/src/main/java/com/quintor/api/ApiApplication.java
+++ api/src/main/java/com/quintor/api/ApiApplication.java
@@ -3,16 +3,10 @@ package com.quintor.api;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 
 @SpringBootApplication
 //@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
-@EnableJpaRepositories(basePackages = "com.quintor.api.repository")
-@EnableMongoRepositories(basePackages = "com.quintor.api.mongoConnection")
-@EntityScan("com.quintor.api.databojects")
 public class ApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
